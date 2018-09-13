1b8ce603a449bd982cce973a030df8130d300b4d
Ubuntu <eyesonalz@data-preprocessing-vm.yklk2kkjyp0uffien4jomnjepc.ax.internal.cloudapp.net>
2018-09-13 09:17:59 +0000

    post process paths

--- postProcess.py
+++ postProcess.py
@@ -1,16 +1,18 @@
 import os
-def postProcess(stack_path):
+def postProcess(h5_path):
     import matlab.engine
     import os
     eng = matlab.engine.start_matlab()
     eng.cd('api/resources/preprocessing/DeepVess', nargout=0)
 
     # TODO change all those strange file names into something consistent
-    path, h5file_path = os.path.split(os.path.abspath(stack_path))
-    output_path = h5file_path.split("noMotion-Ch4-8bit-")[1].split('V_fwd.mat')[0]
+    path, h5file_path = os.path.split(os.path.abspath(h5_path))
     main_filename_part = h5file_path[:-3]
-    mask_file_path = main_filename_part, "-masked.tif"
+    output_path = os.path.join(path, main_filename_part.split("noMotion-Ch4-8bit-")[1] + '.mat')
+    mask_file_path = os.path.join(path, main_filename_part+ "-masked.tif")
 
-    full_mat_filename = os.path.join(path, main_filename_part, "-V_fwd.mat")
-    eng.postProcess(path, h5file_path + ".h5", output_path + ".mat", nargout=0)
-    eng.postProcess(stack_path, output_path, mask_file_path, full_mat_filename)
+    full_mat_filename = os.path.join(path, main_filename_part+ "-V_fwd.mat")
+
+    print(h5_path, output_path, mask_file_path, full_mat_filename,'stack_path, output_path, mask_file_path, full_mat_filename')
+    #eng.postProcess(path, h5file_path + ".h5", output_path + ".mat", nargout=0)
+    eng.postProcess(os.path.join(path, h5_path), output_path, mask_file_path, full_mat_filename, nargout=0)
